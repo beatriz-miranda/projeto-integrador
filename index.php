10b66df4501bd2b673853ef49f83210fd2e7e04a
Kaê Angeli Coutinho <kaeangelicoutinho@Kaes-MacBook-Pro.local>
2019-11-20 19:08:08 -0300

    ADD - Blank style.css

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+	// Titulo da página
+	$title = "Unframed";
+
+	// A FAZER: TROCAR INCLUDE POR REQUIRE QND FRONT FINALIZADO
+	include "assets/inc/head.php";
+	include "assets/inc/navbar.php";
+	include "assets/inc/index.principal.php";
+	include "assets/inc/index.sobre_nos.php";
+	include "assets/inc/footer.php";
