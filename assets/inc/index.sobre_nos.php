48801a0a9415847d1bc18a89074796222287253a
Larissa Coutinho <lariacoutinho@gmail.com>
2019-11-24 18:34:19 -0300

    Update - CSS com media queries de forms-btns e adicionado o hover. Finalizado sobre nós + participe. Adicionado paleta de cores.

--- assets/inc/index.sobre_nos.php
+++ assets/inc/index.sobre_nos.php
@@ -11,25 +11,25 @@
         </div>
 
         <div class="col-12 col-md-4 p-1 p-md-5 d-flex align-items-center" id="bg-sobre-nos">
-            <div class="d-flex flex-column align-items-left m-5">
-                <h1 class="m-0">Sobre<span class="d-block">a loja</span></h1>
-                <p class="text-left my-4 my-md-5 cor-texto-padrao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ut distinctio illo, saepe quas assumenda. Velit nam doloremque necessitatibus odit corporis minus dolores, saepe placeat voluptatibus temporibus quia sunt labore!</p>
+            <div class="d-flex flex-column align-items-left my-5 mx-4">
+                <h1 class="m-0">Sobre <span class="d-md-block">a loja</span></h1>
+                <p class="text-justify my-4 my-md-5 cor-texto-padrao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ut distinctio illo, saepe quas assumenda. Velit nam doloremque necessitatibus odit corporis minus dolores, saepe placeat voluptatibus temporibus quia sunt labore!</p>
                 <button type="button" class="align-self-center align-self-md-start btn btn-padrao">saiba mais<i class="fa fa-arrow-right ml-2"></i></button>
             </div>
         </div>
 
     </div>
 
-    <div class="row py-5">
+    <div class="row">
 
-        <div class="col-12">
-            <form class="d-flex flex-column align-items-center form-padrao" method="POST" action="#">
-                <div class="cor-texto-padrao text-left mb-5">
-                    <h1>Participe</h1>
+        <div class="col-12 p-1 p-md-0">
+            <form class="d-flex flex-column align-items-center my-5 mx-4 form-padrao" method="POST" action="#">
+                <div class="cor-texto-padrao text-justify mb-4 mb-md-5">
+                    <h1 class="m-0">Participe</h1>
                     <p class="my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ut distinctio illo, saepe quas assumenda. Velit nam doloremque necessitatibus odit corporis minus dolores, saepe placeat voluptatibus temporibus quia sunt labore!</p>
                     <input class="input-padrao" type="email" name="email_participe" placeholder="email">
                 </div>
-                <button type="button" class="btn btn-padrao">saiba mais<i class="fa fa-arrow-right ml-2"></i></button>
+                <button type="button" class="mt-2 mt-md-0 btn btn-padrao">inscreva-se<i class="fa fa-arrow-right ml-2"></i></button>
             </form>
         </div>
 
