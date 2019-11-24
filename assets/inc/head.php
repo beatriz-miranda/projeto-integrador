3a315e9c3f7c1a2c986fceb6c6bcaef8474fc85a
Larissa Coutinho <lariacoutinho@gmail.com>
2019-11-24 17:27:32 -0300

    Update - Implementação inicial da página sobre nós e particpe. Adicionado alguma classes padrão no CSS. Adcionado o CDN do font-awesome no head.

--- assets/inc/head.php
+++ assets/inc/head.php
@@ -26,6 +26,8 @@
 
 		<!-- CDN Bootstrap 4.3.1 (css) -->
 		<link rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+		<!-- CDN Font Awesome 4.7.0 (css) -->
+		<link rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">	
 		<!-- style.css global do projeto (compartilhado por todas as páginas) -->
 		<link rel="stylesheet" type="text/css" href=<?='"'. ROOT_PATH . 'assets/css/style.css"'?>>
 	</head>
